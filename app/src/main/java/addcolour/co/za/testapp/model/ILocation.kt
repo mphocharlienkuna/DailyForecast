33f1375eb67bbe1649cd957fc99b386680773b5d
mcnkuna <mpho@cobiinteractive.com>
2018-08-26 11:37:36 +0200

    ILocation Constructor

--- app/src/main/java/addcolour/co/za/testapp/model/ILocation.kt
+++ app/src/main/java/addcolour/co/za/testapp/model/ILocation.kt
@@ -2,15 +2,4 @@ package addcolour.co.za.testapp.model
 
 import com.google.gson.annotations.SerializedName
 
-class ILocation {
-
-    @SerializedName("lat")
-    var latitude: Double = 0.toDouble()
-    @SerializedName("lon")
-    var longitude: Double = 0.toDouble()
-
-    constructor(latitude: Double, longitude: Double) {
-        this.latitude = latitude
-        this.longitude = longitude
-    }
-}
+class ILocation(@SerializedName("lat") var latitude: Double, @SerializedName("lon") var longitude: Double)
