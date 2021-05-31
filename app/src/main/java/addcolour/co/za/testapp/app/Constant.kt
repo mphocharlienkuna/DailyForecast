5bde8f2d62ea2527fd751a1b1406d0e704250f1f
Mpho Charlie Nkuna <mphocharlienkuna@gmail.com>
2021-05-31 18:59:17 +0200

    First Commit
    First Commit

--- app/src/main/java/addcolour/co/za/testapp/app/Constant.kt
+++ app/src/main/java/addcolour/co/za/testapp/app/Constant.kt
@@ -8,7 +8,7 @@ interface Constant {
         const val LOCATION_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION
 
         const val BASE_URL = "https://api.openweathermap.org"
-        const val API_KEY = "53f9d8e4213222cf517d86dc406d67fc"
+        const val API_KEY = ""//TODO Api Key
 
         const val UNIT = "metric"
 
--- app/src/main/java/addcolour/co/za/testapp/model/ILocation.kt
+++
@@ -1,5 +0,0 @@
-package addcolour.co.za.testapp.model
-
-import com.google.gson.annotations.SerializedName
-
-class ILocation(@SerializedName("lat") var latitude: Double, @SerializedName("lon") var longitude: Double)
